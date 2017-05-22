a83e532ed259ae50967655938bf0d0c20d91629f
David Borrego Gutiérrez <dbgjerez@gmail.com>
2017-05-22 08:20:52 +0200

    Inizializando

---
+++ src/main/java/com/dbg/ic/gradle/GradleApplication.java
@@ -0,0 +1,12 @@
+package com.dbg.ic.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleApplication.class, args);
+	}
+}
