736de2d4a8fc21b586048212df16855fd39214ab
David Borrego <dbgjerez@gmail.com>
2017-05-23 23:54:04 +0200

    controlador llamando a los servicios

--- src/main/java/com/dbg/ic/gradle/controller/IndexController.java
+++ src/main/java/com/dbg/ic/gradle/controller/IndexController.java
@@ -1,16 +1,25 @@
 package com.dbg.ic.gradle.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dbg.ic.gradle.dto.HelloWorldDTO;
+import com.dbg.ic.gradle.service.HelloWorldService;
 
 @RestController
 @RequestMapping(value = "/")
 public class IndexController {
 
+	@Autowired
+	private HelloWorldService helloWorldService;
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String index() {
-		return "Hello World";
+	public List<HelloWorldDTO> index() {
+		return helloWorldService.listAll();
 	}
 
 }
