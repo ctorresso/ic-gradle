7c772a6aa5e89ca5969e06bbb05ea799fca1d92e
David Borrego Gutiérrez <dbgjerez@gmail.com>
2017-05-24 11:48:16 +0200

    Devuelvo un objeto en vez de devolver un string

--- src/main/java/com/dbg/ic/gradle/controller/IndexController.java
+++ src/main/java/com/dbg/ic/gradle/controller/IndexController.java
@@ -4,13 +4,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dbg.ic.gradle.dto.HelloWorldDTO;
+
 @RestController
 @RequestMapping(value = "/")
 public class IndexController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String index() {
-		return "Hello world";
+	public HelloWorldDTO index() {
+		return new HelloWorldDTO("es", "Hola mundo!");
 	}
 
 }
