b0cf87fab3310b50482db6ef7f1dd19790cdfd20
David Borrego Gutiérrez <dbgjerez@gmail.com>
2017-05-24 12:13:36 +0200

    test de itnegeración y mock

--- src/test/java/com/dbg/ic/gradle/test/controller/TestIndexController.java
+++ src/test/java/com/dbg/ic/gradle/test/controller/TestIndexController.java
@@ -1,13 +1,18 @@
 package com.dbg.ic.gradle.test.controller;
 
-import org.junit.Ignore;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.dbg.ic.gradle.controller.IndexController;
+import com.dbg.ic.gradle.dto.HelloWorldDTO;
 import com.dbg.ic.gradle.service.HelloWorldService;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -20,23 +25,12 @@ public class TestIndexController {
 	private IndexController indexController;
 
 	@Test
-	@Ignore
 	public void testIndexAll() {
-		// Mockito.when(helloWorldService.listAll()).thenReturn(Arrays.asList(new
-		// HelloWorldDTO("es", "Hola mundo!")));
-		// final List<HelloWorldDTO> l = indexController.index();
-		// Assert.assertTrue(l instanceof List);
-		// Assert.assertEquals(l.get(0).getLanguage(), "es");
-		// Assert.assertEquals(l.get(0).getText(), "Hola mundo!");
-	}
-
-	@Test
-	@Ignore
-	public void testFindByLanguage() {
-		// Mockito.when(helloWorldService.listByLanguage("es")).thenReturn(new
-		// HelloWorldDTO("es", "Hola mundo!"));
-		// Assert.assertEquals(indexController.list("es").getText(), "Hola
-		// mundo!");
+		Mockito.when(helloWorldService.listAll()).thenReturn(Arrays.asList(new HelloWorldDTO("es", "Hola mundo!")));
+		final List<HelloWorldDTO> l = indexController.index();
+		Assert.assertTrue(l instanceof List);
+		Assert.assertEquals(l.get(0).getLanguage(), "es");
+		Assert.assertEquals(l.get(0).getText(), "Hola mundo!");
 	}
 
 }
