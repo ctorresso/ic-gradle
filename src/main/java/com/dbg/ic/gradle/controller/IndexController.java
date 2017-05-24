d8f7821406f17fc8d62e98f12b32254e6e18bb27
David Borrego Gutiérrez <dbgjerez@gmail.com>
2017-05-24 06:41:31 +0200

    dejo vacío

--- src/main/java/com/dbg/ic/gradle/controller/IndexController.java
+++ src/main/java/com/dbg/ic/gradle/controller/IndexController.java
@@ -1,31 +1,16 @@
 package com.dbg.ic.gradle.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.dbg.ic.gradle.dto.HelloWorldDTO;
-import com.dbg.ic.gradle.service.HelloWorldService;
 
 @RestController
 @RequestMapping(value = "/")
 public class IndexController {
 
-	@Autowired
-	private HelloWorldService helloWorldService;
-
 	@RequestMapping(method = RequestMethod.GET)
-	public List<HelloWorldDTO> index() {
-		return helloWorldService.listAll();
-	}
-
-	@RequestMapping(value = "{language}", method = RequestMethod.GET)
-	public HelloWorldDTO list(@PathVariable String language) {
-		return helloWorldService.listByLanguage(language);
+	public String index() {
+		return "Hello world";
 	}
 
 }
--- src/test/java/com/dbg/ic/gradle/test/controller/TestIndexController.java
+++ src/test/java/com/dbg/ic/gradle/test/controller/TestIndexController.java
@@ -1,18 +1,13 @@
 package com.dbg.ic.gradle.test.controller;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.dbg.ic.gradle.controller.IndexController;
-import com.dbg.ic.gradle.dto.HelloWorldDTO;
 import com.dbg.ic.gradle.service.HelloWorldService;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -25,18 +20,23 @@ public class TestIndexController {
 	private IndexController indexController;
 
 	@Test
+	@Ignore
 	public void testIndexAll() {
-		Mockito.when(helloWorldService.listAll()).thenReturn(Arrays.asList(new HelloWorldDTO("es", "Hola mundo!")));
-		final List<HelloWorldDTO> l = indexController.index();
-		Assert.assertTrue(l instanceof List);
-		Assert.assertEquals(l.get(0).getLanguage(), "es");
-		Assert.assertEquals(l.get(0).getText(), "Hola mundo!");
+		// Mockito.when(helloWorldService.listAll()).thenReturn(Arrays.asList(new
+		// HelloWorldDTO("es", "Hola mundo!")));
+		// final List<HelloWorldDTO> l = indexController.index();
+		// Assert.assertTrue(l instanceof List);
+		// Assert.assertEquals(l.get(0).getLanguage(), "es");
+		// Assert.assertEquals(l.get(0).getText(), "Hola mundo!");
 	}
 
 	@Test
+	@Ignore
 	public void testFindByLanguage() {
-		Mockito.when(helloWorldService.listByLanguage("es")).thenReturn(new HelloWorldDTO("es", "Hola mundo!"));
-		Assert.assertEquals(indexController.list("es").getText(), "Hola mundo!");
+		// Mockito.when(helloWorldService.listByLanguage("es")).thenReturn(new
+		// HelloWorldDTO("es", "Hola mundo!"));
+		// Assert.assertEquals(indexController.list("es").getText(), "Hola
+		// mundo!");
 	}
 
 }
